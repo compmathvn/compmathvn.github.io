0eb76c90ccb2bd92a180b3c5b4ca71d767fd4b5e
Duy-Nguyen Ta <thduynguyen@gmail.com>
2016-02-22 08:13:21 -0500

    Nikola auto commit.
    Nikola version: v7.7.6

--- conf.py
+++ conf.py
@@ -687,7 +687,8 @@ CONTENT_FOOTER_FORMATS = {
 # systems.  The following comment systems are supported by Nikola:
 #   disqus, facebook, googleplus, intensedebate, isso, livefyre, muut
 # You can leave this option blank to disable comments.
-COMMENT_SYSTEM = "googleplus"
+# COMMENT_SYSTEM = "googleplus"
+COMMENT_SYSTEM = "disqus"
 # And you also need to add your COMMENT_SYSTEM_ID which
 # depends on what comment system you use. The default is
 # "nikolademo" which is a test account for Disqus. More information
