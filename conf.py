cab703ab8bf694db5867007f5f0a5b938a6ac2df
Duy-Nguyen Ta <thduynguyen@gmail.com>
2016-02-22 22:44:46 -0500

    Nikola auto commit.
    Nikola version: v7.7.6

--- conf.py
+++ conf.py
@@ -700,7 +700,7 @@ COMMENT_SYSTEM_ID = "compmathvn"
 # setting the "annotations" metadata.
 # If set to True, you can disable them for individual posts and pages using
 # the "noannotations" metadata.
-# ANNOTATIONS = False
+ANNOTATIONS = True
 
 # Create index.html for page (story) folders?
 # WARNING: if a page would conflict with the index file (usually
